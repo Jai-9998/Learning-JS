05b156f342b9dccce071d37daa0d75f61abaf02b
Jai-9998 <jai.parkar99@gmail.com>
2025-01-26 05:52:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3+3) // we are using node js, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Jai")
+
+let name = "Jai"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// Primitive Data Types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => to find uniqueness
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
