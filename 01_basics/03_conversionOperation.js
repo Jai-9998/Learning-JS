14f697f983176c7fbae60fcf10b4379b54202c70
Jai-9998 <jai.parkar99@gmail.com>
2025-01-26 08:48:50 +0000

    more discussion on conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,8 +27,64 @@ let isLoggedIn = "Jai"
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** Operations ****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Jai"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+let updatedGameCounter = gameCounter++
+// console.log(updatedGameCounter); 
+
+let x = 10 
+let y = x++ 
+console.log(x); 
+let z = ++x 
+console.log(x);
+console.log(y); 
+console.log(z);
+
+// x++ => first print then increment (postfix) 
+// ++x => first increment then print (prefix)
+
+
+
+
+
+
+
+
+
 
 
 
