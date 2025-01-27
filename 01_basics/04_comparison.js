38915450da781cf079b356e2a7107a2285b54434
Jai-9998 <jai.parkar99@gmail.com>
2025-01-27 17:09:24 +0000

    discussion on comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// Here == (equality check) and >=,<=,>,< (comparisons) works differently. Comparisons convert null to a number, treating to a 0. 
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// The above comparisons should be avoided
+
+// === -> checks the value as well as the datatype
+
+console.log("2" === 2);
+
+
+
